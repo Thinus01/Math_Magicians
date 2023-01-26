472a54f74d2f6753853a2059603f628c5a50f81c
Thinus <thinusvdv2001@gmail.com>
2023-01-26 14:07:10 +0200

    Convert App to class

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import './App.css';
 import './components/style.css';
+import React, { PureComponent } from 'react';
 import Calculate from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Calculate />
-    </div>
-  );
+class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <Calculate />
+      </div>
+    );
+  }
 }
 
 export default App;
