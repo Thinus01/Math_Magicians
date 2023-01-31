508fc777456f502ca582c450d57c9f942265e50b
Thinus <thinusvdv2001@gmail.com>
2023-01-31 14:22:53 +0200

    Update styles

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,7 +1,7 @@
 const Quote = () => (
   <section>
     <h2 className="SubHead">Quote of the day!</h2>
-    <p className="Lorem">
+    <p className="QuoteText">
       Mathematics is not about numbers, equations, computations,
       or algorithms: its is about understanding. - William Paul Thurston
     </p>
