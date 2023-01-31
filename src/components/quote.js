2c2a5ee108e0d049336a600667eb4324226fc223
Thinus <thinusvdv2001@gmail.com>
2023-01-31 12:04:39 +0200

    Add home and quote pages

---
+++ src/components/quote.js
@@ -0,0 +1,11 @@
+const Quote = () => (
+  <section>
+    <h2 className="SubHead">Quote of the day!</h2>
+    <p className="Lorem">
+      Mathematics is not about numbers, equations, computations,
+      or algorithms: its is about understanding. - William Paul Thurston
+    </p>
+  </section>
+);
+
+export default Quote;
